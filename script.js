8023c48f0cbb456ae714e2feb8ff5ade06d0c327
Qwib-Qwib <jp93rom@orange.fr>
2022-01-31 20:13:00 +0100

    Add a few CSS rules to specify the number of rows and columns in the grid, put the original javascript in a new function which generates a default 16x16 grid, add a bit of code to create a div tile, clone it and fill the grid with it.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const sketchContainer = document.createElement("div");
-document.body.appendChild(sketchContainer);
-sketchContainer.classList.add("sketchContainer");
+generateDefaultGrid()
+
+function generateDefaultGrid() {
+    const sketchContainer = document.createElement("div");
+    document.body.appendChild(sketchContainer);
+    sketchContainer.classList.add("sketchContainer");
+    const gridTile = document.createElement("div");
+    sketchContainer.appendChild(gridTile);
+    gridTile.classList.add("gridTile");
+    let clonedTile;
+    for (let i = 0; i < 16*16-1; i++) {
+        clonedTile = gridTile.cloneNode();                  //On ne peut pas copier un node en répétant les appendChild, autrement ça ne manipulera qu'une seule instance du node. IL faut le cloner.
+        sketchContainer.appendChild(clonedTile);
+    }
+}
