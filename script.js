b5846b5008be199b41807b99732195426e9f89f5
Qwib-Qwib <jp93rom@orange.fr>
2022-02-07 20:32:19 +0100

    Fix a missing semicolon in line 4 of the script. Create three new functions which prompt the user for a resize value for the grid, remove all tiles from the grid and generate a new grid according the the user value. Call the first one at the end of reset(). WARNING: Values above 60 make the browser chug or crash. Investigate.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 generateDefaultGrid();
 placeEventListenersOnTiles();
 const button = document.querySelector(".reset");
-button.addEventListener("click", reset)
+button.addEventListener("click", reset);
 
 function generateDefaultGrid() {
     const sketchContainer = document.createElement("div");
@@ -33,4 +33,42 @@ function reset() {
     allTiles.forEach(element => {
         element.classList.remove("tileHovered");
     });
+    resizeGrid();
+}
+
+function resizeGrid() {
+    let newSize = prompt("How many squares do you want per side of the Etch-a-Sketch?", "Please insert a number inferior or equal to 100.");
+    if (newSize > 100) {
+        newSize = prompt("Sorry, that number is too high!", "Please insert a number inferior or equal to 100.");
+    } else if (Math.sign(newSize) == 0 || Math.sign(newSize) == -1) {                                                                           //Ne pas oublier que les returns de prompt sont des strings, pas des chiffres !
+        newSize = prompt("Only integers between 1 and 100, please!", "Please insert a number inferior or equal to 100.");
+    } else if (newSize === null) {
+        generateDefaultGrid();
+    } else {
+        const styleSheet = document.styleSheets[0];
+        styleSheet.cssRules[2].style.gridTemplateRows=`repeat(${newSize}, auto)`;
+        styleSheet.cssRules[2].style.gridTemplateColumns=`repeat(${newSize}, auto)`;
+        removeAllTiles();
+        generateUserGrid(newSize);
+    }
+}
+
+function removeAllTiles() {
+    let container = document.querySelector(".sketchContainer");
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+function generateUserGrid(newSize) {
+    const sketchContainer = document.getElementsByClassName("sketchContainer");
+    const gridTile = document.createElement("div");
+    sketchContainer[0].appendChild(gridTile);
+    gridTile.classList.add("gridTile");
+    let clonedTile;
+    for (let i = 0; i < newSize*newSize-1; i++) {
+        clonedTile = gridTile.cloneNode();                  //On ne peut pas copier un node en répétant les appendChild, autrement ça ne manipulera qu'une seule instance du node. IL faut le cloner.
+        sketchContainer[0].appendChild(clonedTile);
+        placeEventListenersOnTiles();
+    }
 }
