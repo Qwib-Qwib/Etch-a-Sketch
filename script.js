3ddca4b2831fea09372ebad83781ce5578e372a4
Qwib-Qwib <jp93rom@orange.fr>
2022-02-13 20:55:37 +0100

    Edit changeColor() so that it assigns a random color on the first hover, and darkens the tile by 10% on subsequent hovers.

--- script.js
+++ script.js
@@ -25,45 +25,79 @@ function placeEventListenersOnTiles() {
 }
 
 function changeColor() {
-    let randomColor = "#";
-    let randomValue;
-    for (let i = 0; i < 6; i++) {                       // On veut 6 itérations pour faire un code HEX complet, on entre donc i < 6.
-        randomValue = Math.floor(Math.random()*16);     // On a 16 possibilités de caractères différentes pour chaque caractère du code HEX, donc on multiplie le random par 16.
-        if (randomValue == 0) {
-            randomColor += "0";
-        } else if (randomValue == 1) {
-            randomColor += "1";
-        } else if (randomValue == 2) {
-            randomColor += "2";
-        } else if (randomValue == 3) {
-            randomColor += "3";
-        } else if (randomValue == 4) {
-            randomColor += "4";
-        } else if (randomValue == 5) {
-            randomColor += "5";
-        } else if (randomValue == 6) {
-            randomColor += "6";
-        } else if (randomValue == 7) {
-            randomColor += "7";
-        } else if (randomValue == 8) {
-            randomColor += "8";
-        } else if (randomValue == 9) {
-            randomColor += "9";
-        } else if (randomValue == 10) {
-            randomColor += "a";
-        } else if (randomValue == 11) {
-            randomColor += "b";
-        } else if (randomValue == 12) {
-            randomColor += "c";
-        } else if (randomValue == 13) {
-            randomColor += "d";
-        } else if (randomValue == 14) {
-            randomColor += "e";
-        } else if (randomValue == 15) {
-            randomColor += "f";
+    if (this.style.backgroundColor && this.hasAttribute("data-red") == false) {
+        let rValue;
+        let gValue;
+        let bValue;
+        let currentColorValue = this.getAttribute("style");
+        let openParenthesis = currentColorValue.indexOf("(");
+        let firstComma = currentColorValue.indexOf(",");
+        let secondComma = currentColorValue.lastIndexOf(",");
+        let closedParenthesis = currentColorValue.indexOf(")");
+        rValue = currentColorValue.slice(openParenthesis + 1, firstComma);
+        gValue = currentColorValue.slice(firstComma + 2, secondComma);
+        bValue = currentColorValue.slice(secondComma + 2, closedParenthesis);
+        this.dataset.red = rValue;
+        this.dataset.green = gValue;
+        this.dataset.blue = bValue;
+        this.style.backgroundColor=`rgb(${rValue - (rValue/10)}, ${gValue - (gValue/10)}, ${bValue - (bValue/10)})`;
+    } else if (this.style.backgroundColor && this.hasAttribute("data-red") == true) {
+        let rValue;
+        let gValue;
+        let bValue;
+        let currentColorValue = this.getAttribute("style");
+        let openParenthesis = currentColorValue.indexOf("(");
+        let firstComma = currentColorValue.indexOf(",");
+        let secondComma = currentColorValue.lastIndexOf(",");
+        let closedParenthesis = currentColorValue.indexOf(")");
+        rValue = currentColorValue.slice(openParenthesis + 1, firstComma);
+        gValue = currentColorValue.slice(firstComma + 2, secondComma);
+        bValue = currentColorValue.slice(secondComma + 2, closedParenthesis);
+        const rValueOriginal = this.getAttribute("data-red");
+        const gValueOriginal = this.getAttribute("data-green");
+        const bValueOriginal = this.getAttribute("data-blue");
+        this.style.backgroundColor=`rgb(${rValue - (rValueOriginal/10)}, ${gValue - (gValueOriginal/10)}, ${bValue - (bValueOriginal/10)})`;
+    } else {
+        let randomColor = "#";
+        let randomValue;
+        for (let i = 0; i < 6; i++) {                       // On veut 6 itérations pour faire un code HEX complet, on entre donc i < 6.
+            randomValue = Math.floor(Math.random()*16);     // On a 16 possibilités de caractères différentes pour chaque caractère du code HEX, donc on multiplie le random par 16.
+            if (randomValue == 0) {
+                randomColor += "0";
+            } else if (randomValue == 1) {
+                randomColor += "1";
+            } else if (randomValue == 2) {
+                randomColor += "2";
+            } else if (randomValue == 3) {
+                randomColor += "3";
+            } else if (randomValue == 4) {
+                randomColor += "4";
+            } else if (randomValue == 5) {
+                randomColor += "5";
+            } else if (randomValue == 6) {
+                randomColor += "6";
+            } else if (randomValue == 7) {
+                randomColor += "7";
+            } else if (randomValue == 8) {
+                randomColor += "8";
+            } else if (randomValue == 9) {
+                randomColor += "9";
+            } else if (randomValue == 10) {
+                randomColor += "a";
+            } else if (randomValue == 11) {
+                randomColor += "b";
+            } else if (randomValue == 12) {
+                randomColor += "c";
+            } else if (randomValue == 13) {
+                randomColor += "d";
+            } else if (randomValue == 14) {
+                randomColor += "e";
+            } else if (randomValue == 15) {
+                randomColor += "f";
+            }
         }
+        this.style.backgroundColor=`${randomColor}`;
     }
-    this.style.backgroundColor=`${randomColor}`;
 }
 
 function reset() {
