72b2c4930e49f5eb4dd1a2e569abdf40785aa0cd
Julien Pons <jp93rom@orange.fr>
2023-07-18 18:15:38 +0200

    Refactor code: much DRIer, much clearer. Shorter too. Fix tiles not becoming entirely black on the tenth passage.

--- script.js
+++ script.js
@@ -4,144 +4,144 @@ const button = document.querySelector(".reset");
 button.addEventListener("click", reset);
 
 function generateDefaultGrid() {
-    const sketchContainer = document.createElement("div");
-    document.body.appendChild(sketchContainer);
-    sketchContainer.classList.add("sketchContainer");
-    const gridTile = document.createElement("div");
-    sketchContainer.appendChild(gridTile);
-    gridTile.classList.add("gridTile");
-    let clonedTile;
-    for (let i = 0; i < 16*16-1; i++) {
-        clonedTile = gridTile.cloneNode();                  //On ne peut pas copier un node en répétant les appendChild, autrement ça ne manipulera qu'une seule instance du node. IL faut le cloner.
-        sketchContainer.appendChild(clonedTile);
-    }
+  const sketchContainer = generateGridContainer();
+  populateGridContainer(sketchContainer);
+}
+
+function generateGridContainer() {
+  const sketchContainer = document.createElement("div");
+  document.body.appendChild(sketchContainer);
+  sketchContainer.classList.add("sketchContainer");
+  return sketchContainer;
+}
+
+function populateGridContainer(container, gridSize=16) {
+  const gridTile = createTileTemplate(container);
+  populateWithTileClones(gridTile, container, gridSize);
+}
+
+function createTileTemplate(container) {
+  const gridTile = document.createElement("div");
+  container.appendChild(gridTile);
+  gridTile.classList.add("gridTile");
+  return gridTile;
+}
+
+function populateWithTileClones(tileTemplate, container, gridSize) {
+  let clonedTile;
+  for (let i = 0; i < gridSize*gridSize-1; i++) {
+    // On ne peut pas copier un node en répétant les appendChild, autrement ça ne manipulera qu'une seule instance du node. Il faut le cloner.
+    clonedTile = tileTemplate.cloneNode();
+    container.appendChild(clonedTile);
+  }
 }
 
 function placeEventListenersOnTiles() {
-    let allTiles = document.querySelectorAll(".gridTile");
-    allTiles.forEach(element => {
-        element.addEventListener("mouseover", changeColor);
-    });
+  let allTiles = document.querySelectorAll(".gridTile");
+  allTiles.forEach(element => {
+    element.addEventListener("mouseover", changeColor);
+  });
 }
 
 function changeColor() {
-    if (this.style.backgroundColor && this.hasAttribute("data-red") == false) {
-        let rValue;
-        let gValue;
-        let bValue;
-        let currentColorValue = this.getAttribute("style");
-        let openParenthesis = currentColorValue.indexOf("(");
-        let firstComma = currentColorValue.indexOf(",");
-        let secondComma = currentColorValue.lastIndexOf(",");
-        let closedParenthesis = currentColorValue.indexOf(")");
-        rValue = currentColorValue.slice(openParenthesis + 1, firstComma);
-        gValue = currentColorValue.slice(firstComma + 2, secondComma);
-        bValue = currentColorValue.slice(secondComma + 2, closedParenthesis);
-        this.dataset.red = rValue;
-        this.dataset.green = gValue;
-        this.dataset.blue = bValue;
-        this.style.backgroundColor=`rgb(${rValue - (rValue/10)}, ${gValue - (gValue/10)}, ${bValue - (bValue/10)})`;
-    } else if (this.style.backgroundColor && this.hasAttribute("data-red") == true) {
-        let rValue;
-        let gValue;
-        let bValue;
-        let currentColorValue = this.getAttribute("style");
-        let openParenthesis = currentColorValue.indexOf("(");
-        let firstComma = currentColorValue.indexOf(",");
-        let secondComma = currentColorValue.lastIndexOf(",");
-        let closedParenthesis = currentColorValue.indexOf(")");
-        rValue = currentColorValue.slice(openParenthesis + 1, firstComma);
-        gValue = currentColorValue.slice(firstComma + 2, secondComma);
-        bValue = currentColorValue.slice(secondComma + 2, closedParenthesis);
-        const rValueOriginal = this.getAttribute("data-red");
-        const gValueOriginal = this.getAttribute("data-green");
-        const bValueOriginal = this.getAttribute("data-blue");
-        this.style.backgroundColor=`rgb(${rValue - (rValueOriginal/10)}, ${gValue - (gValueOriginal/10)}, ${bValue - (bValueOriginal/10)})`;
-    } else {
-        let randomColor = "#";
-        let randomValue;
-        for (let i = 0; i < 6; i++) {                       // On veut 6 itérations pour faire un code HEX complet, on entre donc i < 6.
-            randomValue = Math.floor(Math.random()*16);     // On a 16 possibilités de caractères différentes pour chaque caractère du code HEX, donc on multiplie le random par 16.
-            if (randomValue == 0) {
-                randomColor += "0";
-            } else if (randomValue == 1) {
-                randomColor += "1";
-            } else if (randomValue == 2) {
-                randomColor += "2";
-            } else if (randomValue == 3) {
-                randomColor += "3";
-            } else if (randomValue == 4) {
-                randomColor += "4";
-            } else if (randomValue == 5) {
-                randomColor += "5";
-            } else if (randomValue == 6) {
-                randomColor += "6";
-            } else if (randomValue == 7) {
-                randomColor += "7";
-            } else if (randomValue == 8) {
-                randomColor += "8";
-            } else if (randomValue == 9) {
-                randomColor += "9";
-            } else if (randomValue == 10) {
-                randomColor += "a";
-            } else if (randomValue == 11) {
-                randomColor += "b";
-            } else if (randomValue == 12) {
-                randomColor += "c";
-            } else if (randomValue == 13) {
-                randomColor += "d";
-            } else if (randomValue == 14) {
-                randomColor += "e";
-            } else if (randomValue == 15) {
-                randomColor += "f";
-            }
-        }
-        this.style.backgroundColor=`${randomColor}`;
-    }
+  if (parseInt(this.dataset.darkenings) >= 9) {
+    ultimateDarken(this);
+  } else if (this.style.backgroundColor && this.hasAttribute("data-red") == false) {
+    firstDarken(this);
+  } else if (this.style.backgroundColor && this.hasAttribute("data-red") == true) {
+    repeatDarken(this);
+  } else {
+    colorizeTile(this);
+  }
+}
+
+function firstDarken(tile) {
+  let rValue, gValue, bValue;
+  [rValue, gValue, bValue] = retrieveTileColorValues(tile);
+  tile.dataset.red = rValue;
+  tile.dataset.green = gValue;
+  tile.dataset.blue = bValue;
+  tile.dataset.darkenings = 1;
+  tile.style.backgroundColor=`rgb(${rValue - (rValue/10)}, ${gValue - (gValue/10)}, ${bValue - (bValue/10)})`;
+}
+
+function repeatDarken(tile) {
+  let rValue, gValue, bValue;
+  [rValue, gValue, bValue] = retrieveTileColorValues(tile);
+  const rValueOriginal = tile.getAttribute("data-red");
+  const gValueOriginal = tile.getAttribute("data-green");
+  const bValueOriginal = tile.getAttribute("data-blue");
+  tile.dataset.darkenings = parseInt(tile.dataset.darkenings) + 1;
+  tile.style.backgroundColor=`rgb(${rValue - (rValueOriginal/10)}, ${gValue - (gValueOriginal/10)}, ${bValue - (bValueOriginal/10)})`;
+}
+
+function ultimateDarken(tile) {
+  tile.style.backgroundColor="rgb(0, 0, 0)";
+  tile.dataset.darkenings = 10;
+  tile.removeEventListener("mouseover", changeColor);
+}
+
+function colorizeTile(tile) {
+  let randomColor = "#";
+  let randomValue;
+  // On veut 6 itérations pour faire un code HEX complet, on entre donc i < 6.
+  for (let i = 0; i < 6; i++) {
+    // On a 16 possibilités de caractères différentes pour chaque caractère du code HEX, donc on multiplie le random par 16.
+    randomValue = Math.floor(Math.random()*16);
+    // Transforme un chiffre de 1 à 16 en string pour une valeur hex.
+    randomColor += randomValue.toString(16);
+  }
+  tile.style.backgroundColor=`${randomColor}`;
+}
+
+function retrieveTileColorValues(tile) {
+  let rValue, gValue, bValue;
+  let currentColorValue = tile.getAttribute("style");
+  let openParenthesis = currentColorValue.indexOf("(");
+  let firstComma = currentColorValue.indexOf(",");
+  let secondComma = currentColorValue.lastIndexOf(",");
+  let closedParenthesis = currentColorValue.indexOf(")");
+  rValue = currentColorValue.slice(openParenthesis + 1, firstComma);
+  gValue = currentColorValue.slice(firstComma + 2, secondComma);
+  bValue = currentColorValue.slice(secondComma + 2, closedParenthesis);
+  return [rValue, gValue, bValue];
 }
 
 function reset() {
-    let allTiles = document.querySelectorAll(".gridTile");
-    let allTilesArray = Array.from(allTiles);
-    allTilesArray.forEach(element => {
-        element.removeAttribute("style", "background-color");
-    });
-    resizeGrid();
+  let allTiles = document.querySelectorAll(".gridTile");
+  let allTilesArray = Array.from(allTiles);
+  allTilesArray.forEach(element => {
+    element.removeAttribute("style", "background-color");
+  });
+  resizeGrid();
 }
 
 function resizeGrid() {
-    let newSize = prompt("How many squares do you want per side of the Etch-a-Sketch?", "Please insert a number inferior or equal to 100.");
-    if (newSize > 100) {
-        newSize = prompt("Sorry, that number is too high!", "Please insert a number inferior or equal to 100.");
-    } else if (Math.sign(newSize) === "0" || Math.sign(newSize) == -1) {                                                                           //Ne pas oublier que les returns de prompt sont des strings, pas des chiffres ! Aussi, == 0 peut être interprété comme null alors attention.
-        newSize = prompt("Only integers between 1 and 100, please!", "Please insert a number inferior or equal to 100.");
-    } else if (newSize === null) {
-        return null;
-    } else {
-        const styleSheet = document.styleSheets[0];
-        styleSheet.cssRules[2].style.gridTemplateRows=`repeat(${newSize}, auto)`;
-        styleSheet.cssRules[2].style.gridTemplateColumns=`repeat(${newSize}, auto)`;
-        removeAllTiles();
-        generateUserGrid(newSize);
-        placeEventListenersOnTiles();
-    }
+  let newSize = prompt("How many squares do you want per side of the Etch-a-Sketch?", "Please insert a number inferior or equal to 100.");
+  if (newSize > 100) {
+    newSize = prompt("Sorry, that number is too high!", "Please insert a number inferior or equal to 100.");
+  } else if (Math.sign(newSize) === "0" || Math.sign(newSize) == -1) {                                                                           // Ne pas oublier que les returns de prompt sont des strings, pas des chiffres ! Aussi, == 0 peut être interprété comme null alors attention.
+    newSize = prompt("Only integers between 1 and 100, please!", "Please insert a number inferior or equal to 100.");
+  } else if (newSize === null) {
+    return null;
+  } else {
+    const styleSheet = document.styleSheets[0];
+    styleSheet.cssRules[2].style.gridTemplateRows=`repeat(${newSize}, auto)`;
+    styleSheet.cssRules[2].style.gridTemplateColumns=`repeat(${newSize}, auto)`;
+    removeAllTiles();
+    generateUserGrid(newSize);
+    placeEventListenersOnTiles();
+  }
 }
 
 function removeAllTiles() {
-    let container = document.querySelector(".sketchContainer");
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
+  let container = document.querySelector(".sketchContainer");
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
 }
 
 function generateUserGrid(newSize) {
-    const sketchContainer = document.getElementsByClassName("sketchContainer");
-    const gridTile = document.createElement("div");
-    sketchContainer[0].appendChild(gridTile);
-    gridTile.classList.add("gridTile");
-    let clonedTile;
-    for (let i = 0; i < newSize*newSize-1; i++) {
-        clonedTile = gridTile.cloneNode();                  //On ne peut pas copier un node en répétant les appendChild, autrement ça ne manipulera qu'une seule instance du node. IL faut le cloner.
-        sketchContainer[0].appendChild(clonedTile);
-    }
+  const sketchContainer = document.querySelector(".sketchContainer");
+  populateGridContainer(sketchContainer, newSize);
 }
